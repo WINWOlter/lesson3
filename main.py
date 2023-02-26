9832979f8ea4eb445390bc1e9df144a6118c91c0
WNWolter <113820891+WINWOlter@users.noreply.github.com>
2023-02-26 07:54:29 +0000

    добавив  кота

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 class Person:
   height = 170
-  name = "Name"
+  name = "Mark"
   is_sad = True
-  def__init__(self,name,height):
-  self.name = name
-  self.haight = haight
+  def __init__(self,name,haight):
+   self.name = name
+   self.haight = haight
+
+
+class Vova:
+ height = 174
+ age = 13
+ name = "Vova"
 
 
 
+class Cat:
+ height = 40
+ age = 3
+ name = "Cat"
+ def __init__(self,name,height):
+  self.name = Cat
+  self.age = 3
+  
+  def play_w_human(self,human):
+     human.is_sad = False
 
-me = Person("Mark,13")
+my_pet = Cat("Cat",40)
+me = Person("Mark",170)
 you = Person('Vova', 176)
 
+print(me.is_sad)
+
+my_pet.play_w_hooman(me)
+
+print("Сумний - ",me.is_sad)
+     
+     
+
+     
   
