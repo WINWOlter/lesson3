98488841b5e4f50e75c2447f1b681e253a30c06a
WNWolter <113820891+WINWOlter@users.noreply.github.com>
2023-02-26 07:20:40 +0000

    добавив персону і свого друга

---
+++ main.py
@@ -0,0 +1,15 @@
+class Person:
+  height = 170
+  name = "Name"
+  is_sad = True
+  def__init__(self,name,height):
+  self.name = name
+  self.haight = haight
+
+
+
+
+me = Person("Mark,13")
+you = Person('Vova', 176)
+
+  
